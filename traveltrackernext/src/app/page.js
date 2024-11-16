39cebb531081e0632468bd2b5b2d7418fc54d7b3
Kshitij Kochhar <kshitij@ctfguide.com>
2024-11-16 13:30:53 -0500

    [feat] added a detailed loading page

--- traveltrackernext/src/app/page.js
+++ traveltrackernext/src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-
 import React, { useEffect, useState } from "react";
 import MapDisplay from "../components/MapDisplay";
 
@@ -8,11 +7,14 @@ export default function Home() {
 	const [posts, setPosts] = useState([]);
 	const [lastFetchedData, setLastFetchedData] = useState([]);
 	const [dots, setDots] = useState("");
+	const [loadingTime, setLoadingTime] = useState(0);
 
 	const fetchPosts = async () => {
 		try {
-			const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/posts`);
-      console.log(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/posts`)
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/posts`
+			);
+			console.log(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/posts`);
 			const data = await response.json();
 
 			if (JSON.stringify(data) !== JSON.stringify(lastFetchedData)) {
@@ -38,14 +40,35 @@ export default function Home() {
 		return () => clearInterval(interval);
 	}, []);
 
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setLoadingTime((prevTime) => prevTime + 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
 		<div>
 			{posts.length > 0 ? (
 				<MapDisplay posts={posts} />
 			) : (
-				<div className="flex items-center justify-center h-screen bg-gray-100">
-					<p className="text-xl font-bold text-gray-700">
+				<div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-50 to-blue-100">
+					<p className="text-2xl font-semibold text-gray-800">
 						Loading Map<span>{dots}</span>
+					</p>
+          <p className="text-gray-600 mt-10 text-xl font-bold text-center">
+						Spinning up the backend
+					</p>
+					<p className="text-gray-600 mt-2 text-center">
+						Estimated time:{" "}
+						<span className="font-medium">up to 50 seconds</span>
+					</p>
+					<p className="text-gray-600 mt-1">
+						Elapsed time:{" "}
+						<span className="font-semibold">
+							{loadingTime} second{loadingTime !== 1 ? "s" : ""}
+						</span>
 					</p>
 				</div>
 			)}
